5f09c03f3f3cfcf4baebe6b2e8ced5e47bae2607
Eling Pramuatmaja <eling@eling.id>
2026-03-13 05:46:20 +0800

    feat: replace OMZ's fzf with mattmc3's fzf (zsh_custom)

---
+++ conf.d/fzf.zsh
@@ -0,0 +1,15 @@
+#
+# fzf: Configure the fuzzy finder.
+#
+
+if ! (( $+commands[fzf] )); then
+  echo "fzf not found" >&2
+  return 1
+fi
+
+# https://github.com/ajeetdsouza/fzf
+if (( $+functions[cached-eval] )); then
+  cached-eval 'fzf-zsh' fzf --zsh
+else
+  source <(fzf --zsh)
+fi
