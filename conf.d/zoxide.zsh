f53d84dc3c0ce62a28dbca2186385d184f9424dc
Eling Pramuatmaja <eling@eling.id>
2026-03-13 05:44:52 +0800

    feat: cache zoxide using cached-eval

--- conf.d/zoxide.zsh
+++ conf.d/zoxide.zsh
@@ -3,8 +3,13 @@
 #
 
 if ! (( $+commands[zoxide] )); then
-  return 0
+  echo "zoxide not found" >&2
+  return 1
 fi
 
 # https://github.com/ajeetdsouza/zoxide
-source <(zoxide init zsh)
+if (( $+functions[cached-eval] )); then
+  cached-eval 'zoxide-init-zsh' zoxide init zsh
+else
+  source <(zoxide init zsh)
+fi
