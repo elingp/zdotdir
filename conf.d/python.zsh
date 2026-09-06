1e31cc37bb87b3cccdf75c31e2f2866bafb3d0e1
Eling Pramuatmaja <eling@eling.id>
2026-09-06 23:17:43 +0800

    feat(python): add comprehensive aliases for uv

--- conf.d/python.zsh
+++ conf.d/python.zsh
@@ -13,6 +13,33 @@ function pipup {
   pip list --outdated | cut -d ' ' -f1 | xargs -n1 pip install -U
 }
 
+# uv
+if (( $+commands[uv] )); then
+  alias uv="noglob uv"
+
+  alias uva='uv add'
+  alias uvexp='uv export --format requirements-txt --no-hashes --output-file requirements.txt --quiet'
+  alias uvi='uv init'
+  alias uvinw='uv init --no-workspace'
+  alias uvl='uv lock'
+  alias uvlr='uv lock --refresh'
+  alias uvlu='uv lock --upgrade'
+  alias uvp='uv pip'
+  alias uvpi='uv python install'
+  alias uvpl='uv python list'
+  alias uvpy='uv python'
+  alias uvpp='uv python pin'
+  alias uvpu='uv python uninstall'
+  alias uvr='uv run'
+  alias uvrm='uv remove'
+  alias uvs='uv sync'
+  alias uvsr='uv sync --refresh'
+  alias uvsu='uv sync --upgrade'
+  alias uvtr='uv tree'
+  alias uvup='uv self update'
+  alias uvv='uv venv'
+fi
+
 function pyclean {
   # Clean common python cache files.
   find "${@:-.}" -type f -name "*.py[co]" -delete
