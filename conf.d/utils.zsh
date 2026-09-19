19c280848eeb5609b92e05e81bb2db1cef66072d
Eling Pramuatmaja <eling@eling.id>
2026-09-19 14:52:48 +0800

    fix(utils): activate mise before evaluating bun completions

--- conf.d/utils.zsh
+++ conf.d/utils.zsh
@@ -1,6 +1,6 @@
+cached-eval mise activate zsh # `cached-eval --clear mise activate zsh` if `code` problem arises
 cached-eval bun completions
 # cached-eval histdb init zsh # not installed (need mattmc3's histdb)
-cached-eval mise activate zsh # `cached-eval --clear mise activate zsh` if `code` problem arises
 # cached-eval uv generate-shell-completion zsh # hooked by Brew
 # cached-eval zsh-patina activate # hooked by Z1
 cached-eval zoxide init zsh
